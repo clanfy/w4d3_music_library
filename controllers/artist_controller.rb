926b0bebe9cccf4ad4f807f475828c4515dabac1
Clacson <cookiellacson@gmail.com>
2016-10-19 15:29:07 +0100

    artist controller started

--- controllers/artist_controller.rb
+++ controllers/artist_controller.rb
@@ -1,24 +1,26 @@
-require( 'sinatra' )
-require( 'sinatra/contrib/all' )
-require( 'pry-byebug' )
-require_relative('./models/artist') 
+
+
 
 #INDEX
 get "/artists" do
-
-end
+   @artists = Artist.all()
+   erb(:'artists/index')
+ end
 
 #NEW
 get "/artists/new" do
-
+  erb(:'artists/new')
 end
 
 #CREATE
 post "/artists" do
-
+  @artist = Artist.new(params)
+  @artist.save()
+  erb(:'artists/create')
 end
 
 #SHOW
-GET "/artists/:id" do
-
+get "/artists/:id" do
+  @artist = Artist.find(params[:id])
+  erb(:'artists/show')
 end
